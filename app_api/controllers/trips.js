a17c897722b4c3527e7e2acd14bbe479c22c238e
monti215 <gmmonti215@gmail.com>
2024-11-29 13:01:29 -0500

    Module 5 completed baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr');
+const Model = mongoose.model('trips');
+
+const tripsList = async(req, res) => {
+    const q = await Model
+        .find({'code' : req.params.tripCode })
+        .exec();
+    if(!q)
+    {
+        return res
+                .status(404)
+                .json(err);
+    } else {
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode
+};
