08c074bb6e22821957e8473a0e8e9ab59f1c4359
monti215 <gmmonti215@gmail.com>
2024-12-15 17:47:45 -0500

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
+// Enable CORS
+app.use('/api', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
 //catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
